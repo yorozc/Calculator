677bc95e32970c348903a26b97166e681d06eef5
yorozc <yorozcolop49@gmail.com>
2024-09-03 00:56:54 -0700

    added func to clear button and number buttons

--- calc.js
+++ calc.js
@@ -1,8 +1,25 @@
 let num1 = 0;
 let num2 = 0;
 let operator = null;
+const numDisplay = document.getElementById("display"); 
 
+document.body.addEventListener('click', function(event){
+    if(event.target.tagName === 'BUTTON'){
+        if(event.target.classList.contains('numbers')) {
+            if(numDisplay.textContent === '0'){
+                numDisplay.textContent = '';
+            }
+            //don't let user add more than one decimal
+            const buttonValue = event.target.value;
+            numDisplay.textContent += buttonValue
+        }
+        
+    }
+});
 
+document.getElementById('clear').addEventListener('click', () =>{
+    numDisplay.textContent = '0';
+})
 function add(num1, num2){
     return num1 + num2;
 }
