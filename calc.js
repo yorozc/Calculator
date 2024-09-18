11c9803430dd64cf75d8347a10be6cf326f5158d
yorozc <yorozcolop49@gmail.com>
2024-09-18 16:28:08 -0700

    fixed logic with button that changes the sign of a number

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
-let num1, num2, operator;
+let num1 = '';
+let num2 = '';
+let operator = '';
 let num1flag = true; 
 let num2flag = false;
 let opflag = false;
@@ -12,10 +14,12 @@ document.body.addEventListener('click', function(event){
         if(event.target.classList.contains('numbers') && num1flag) {
             if(numDisplay.textContent === '0'){
                 numDisplay.textContent = '';
-                num1 = 0;
             }
+
             num1 += event.target.value;    
-            console.log(num1);
+
+            console.log("num1:" + num1);
+
             numDisplay.textContent += event.target.value;
         
         } 
@@ -23,13 +27,17 @@ document.body.addEventListener('click', function(event){
         if(event.target.classList.contains('numbers') && num2flag){
             if(opflag){
                 num2 += event.target.value;
-                console.log(num2);
+                console.log("Opflag Num2: " + num2);
+
                 numDisplay.textContent += event.target.value; 
+
             }else{
                 numDisplay.textContent = '';
                 num2 += event.target.value;
                 console.log("num2:" + num2);
+
                 opflag = true;
+                
                 numDisplay.textContent = String(Number(num2));
             }
             if(prevOp.value === "%" || prevOp.value == "pos/neg"){
@@ -42,12 +50,11 @@ document.body.addEventListener('click', function(event){
         }
 
         if(event.target.classList.contains('operator')){
-            numDisplay.textContent = String(Number(num1));
+            numDisplay.textContent = num1;
             operator = event.target.value;
             console.log(operator); 
             num1flag = false;
             num2flag = true;
-            num2 = 0;
             event.target.style.backgroundColor = 'white';
             prevOp = event.target;
             
@@ -57,10 +64,10 @@ document.body.addEventListener('click', function(event){
         if(event.target.classList.contains('equal')){
             let result = operate(Number(num1), Number(num2), operator);
             console.log("result:" + result);
-            num1 = result;
-            num2 = 0;
-            opflag = false;
             numDisplay.textContent = result;
+            num1 = result;
+            num2 = '';
+            opflag = false;
         }
 
     }
@@ -68,8 +75,8 @@ document.body.addEventListener('click', function(event){
 
 document.getElementById('clear').addEventListener('click', () =>{
     numDisplay.textContent = '0';
-    num1 = 0;
-    num2 = 0;
+    num1 = '';
+    num2 = '';
     num1flag = true;
     num2flag = false;
     opflag = false;
@@ -78,8 +85,23 @@ document.getElementById('clear').addEventListener('click', () =>{
     }else{
         prevOp.style.backgroundColor = 'rgb(44 ,78, 128)';
     }
-    prevOp = 'null';
+    prevOp = '';
     
+});
+
+document.getElementById("pos/neg").addEventListener('click', ()=>{
+    numDisplay.textContent = '';
+    if(num1flag){
+        num1 = Number(num1) * -1;
+        console.log("pos/neg" + num1);
+        numDisplay.textContent += num1;
+    }
+
+    else if(num2flag){
+        num2 = Number(num2) * -1;
+        console.log("pos/neg" + num2);
+        numDisplay.textContent += num2;
+    }
 });
 
 function add(num1, num2){
@@ -97,9 +119,6 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
-
-
-
 
 function operate(num1, num2, operator){
     //have switch statement to check which operator it is
